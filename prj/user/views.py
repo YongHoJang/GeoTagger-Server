df755dc490f92513c42d79338bc5aa4f37635efe
Min Seong Kang <spiritbanner@gmail.com>
2014-02-14 15:21:09 -1000

    using lock.

--- prj/user/views.py
+++ prj/user/views.py
@@ -121,21 +121,21 @@ def create_project():
     form = CreateProjectForm(request.form)
     
     if request.method == 'POST' and form.validate():
-        #with lock:
-        proj = Project(prj_name=form.name.data, prj_desc=form.desc.data, 
-            owner=current_user.get_id())
-        proj.save()
-        # Add a user as an owner of a project
-        owner_name = current_user.firstname + ' ' + current_user.lastname
-        proj.add_member(name=owner_name, email=current_user.get_id(), 
-            role=Project.ROLE_OWNER)
-        flash("New project has been created.", category='index_page')
-        # Generate a project owner's appkey & save it to ProjectMemberKey coll.
-        key = utils.generate_appkey(APPKEY_LENGTH)
-        prjmemkey = ProjectMemberKey(prj_id=proj.prj_id, appkey=key, 
-            member_email=proj.owner)
-        prjmemkey.save()
-        return redirect(url_for('.list_projects'))
+        with lock:
+            proj = Project(prj_name=form.name.data, prj_desc=form.desc.data, 
+                owner=current_user.get_id())
+            proj.save()
+            # Add a user as an owner of a project
+            owner_name = current_user.firstname + ' ' + current_user.lastname
+            proj.add_member(name=owner_name, email=current_user.get_id(), 
+                role=Project.ROLE_OWNER)
+            flash("New project has been created.", category='index_page')
+            # Generate a project owner's appkey & save it to ProjectMemberKey coll.
+            key = utils.generate_appkey(APPKEY_LENGTH)
+            prjmemkey = ProjectMemberKey(prj_id=proj.prj_id, appkey=key, 
+                member_email=proj.owner)
+            prjmemkey.save()
+            return redirect(url_for('.list_projects'))
         
     return render_template('create_project.html', form=form)
 
